e91119ee6ff3971826fa62ca809bff3ee5a76d58
nwofeemmanuel1 <enwofe2020@gmail.com>
2022-01-11 11:01:23 +0100

    changed navbar

--- videoRentalFrontend/homeJs/nav.js
+++ videoRentalFrontend/homeJs/nav.js
@@ -6,7 +6,7 @@ document.querySelector("#close").onclick=()=>{
     document.querySelector(".navopen").className="navclose"
 }
 
-document.addEventListener("DOMContentLoaded",()=>document.querySelector("nav").style.display='none')
+document.addEventListener("DOMContentLoaded",()=>document.querySelector("nav").style.display='block')
 window.onscroll=()=>{
     if(window.scrollY ==0){
         document.querySelector("nav").style.display='block'
