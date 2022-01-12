58851ac811212f3bc033880a74eebca4de12f334
nwofeemmanuel1 <enwofe2020@gmail.com>
2022-01-12 10:48:15 +0100

    changed video pages

--- videoRentalFrontend/javascript/login.js
+++ videoRentalFrontend/javascript/login.js
@@ -1,99 +1,219 @@
 
 
-function setCookie(email, token) {
-  // alert("called")
-    const d = new Date();
-    d.setTime(d.getTime() + 24*60*60*1000);
-    let expires = "expires="+ d.toUTCString();
-    document.cookie=`email=${email} ; ${expires}`
-    document.cookie = `token=${token} ; ${expires}`;
-    window.location.href="/videos.html"
-  } 
+// function setCookie(email, token) {
+//   // alert("called")
+//     const d = new Date();
+//     d.setTime(d.getTime() + 24*60*60*1000);
+//     let expires = "expires="+ d.toUTCString();
+//     document.cookie=`email=${email} ; ${expires}`
+//     document.cookie = `token=${token} ; ${expires}`;
+//     window.location.href="/videos.html"
+//   } 
 
 
 
-const loginUser=async(email,password)=>{
+// const loginUser=async(email,password)=>{
  
 
- try{
+//  try{
 
- document.querySelector("#login").innerHTML="proccessing..."
-  const response= await fetch("/api/login",{
-method:"POST",
-headers:{"content-type":"application/json"},
-body:JSON.stringify({email,password})
-    })
-    const result=await response.json()
-    console.log(result)
-    if(result.error){
-      document.querySelector(".errMessage").innerHTML=result.errMessage
-      document.querySelector("#login").innerHTML="try again"
-    }
-     return setCookie(result.user.email,result.token)
-  }catch(err){
-    // document.querySelector(".errMessage").innerHTML="an error occured please try again"
-    document.querySelector("#login").innerHTML="try again"
-  }
-}
+//  document.querySelector("#login").innerHTML="proccessing..."
+//   const response= await fetch("/api/login",{
+// method:"POST",
+// headers:{"content-type":"application/json"},
+// body:JSON.stringify({email,password})
+//     })
+//     const result=await response.json()
+//     console.log(result)
+//     if(result.error){
+//       document.querySelector(".errMessage").innerHTML=result.errMessage
+//       document.querySelector("#login").innerHTML="try again"
+//     }
+//      return setCookie(result.user.email,result.token)
+//   }catch(err){
+//     // document.querySelector(".errMessage").innerHTML="an error occured please try again"
+//     document.querySelector("#login").innerHTML="try again"
+//   }
+// }
 
-//response gotten
+// //response gotten
 
-// loginUser("email@gmail.com","password")
+// // loginUser("email@gmail.com","password")
 
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-      }
-  }
-  return "";
-}
-
-
+// function getCookie(cname) {
+//   let name = cname + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(';');
+//   for (let i = 0; i < ca.length; i++) {
+//       let c = ca[i];
+//       while (c.charAt(0) == ' ') {
+//           c = c.substring(1);
+//       }
+//       if (c.indexOf(name) == 0) {
+//           return c.substring(name.length, c.length);
+//       }
+//   }
+//   return "";
+// }
 
 
-document.addEventListener("DOMContentLoaded",()=>{
-    document.querySelector("#login").onclick=()=>{
-      event.preventDefault()
-    const email=document.querySelector("#email")
-    const password=document.querySelector("#password")
-    if(!email.value)return document.querySelector(".errMessage").innerHTML="Email is required"
-    if(!password.value)return document.querySelector(".errMessage").innerHTML="Password is required"
-    if(password.value.length < 8)return document.querySelector(".errMessage").innerHTML="Password must be greater than 8 characters"
-
-    document.querySelector(".errMessage").innerHTML=""
-
-    loginUser(email.value,password.value)
-    }
-})
 
 
+// document.addEventListener("DOMContentLoaded",()=>{
+//     document.querySelector("#login").onclick=()=>{
+//       event.preventDefault()
+//     const email=document.querySelector("#email")
+//     const password=document.querySelector("#password")
+//     if(!email.value)return document.querySelector(".errMessage").innerHTML="Email is required"
+//     if(!password.value)return document.querySelector(".errMessage").innerHTML="Password is required"
+//     if(password.value.length < 8)return document.querySelector(".errMessage").innerHTML="Password must be greater than 8 characters"
+
+//     document.querySelector(".errMessage").innerHTML=""
+
+//     loginUser(email.value,password.value)
+//     }
+// })
 
 
 
 
 
 
-const signUpButton = document.getElementById("signUp");
-const signInButton =document.getElementById("login");
-const container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
 
-// signInButton.addEventListener("click", () => {
-//   container.classList.remove("right-panel-active");
+// const signUpButton = document.getElementById("signUp");
+// const signInButton =document.getElementById("login");
+// const container = document.getElementById("container");
+
+// signUpButton.addEventListener("click", () => {
+//   container.classList.add("right-panel-active");
 // });
 
-document.addEventListener("DOMContentLoaded",()=>{
-  document.querySelector(".ghost").onclick=()=> container.classList.remove("right-panel-active");
+// // signInButton.addEventListener("click", () => {
+// //   container.classList.remove("right-panel-active");
+// // });
 
+// document.addEventListener("DOMContentLoaded",()=>{
+//   document.querySelector(".ghost").onclick=()=> container.classList.remove("right-panel-active");
+
+// })
+
+
+
+
+
+
+
+
+
+let Redirect;
+
+const setCookie=(email, token,newUser)=> {
+    // alert("called")
+      const d = new Date();
+      d.setTime(d.getTime() + 24*60*60*1000);
+      let expires = "expires="+ d.toUTCString();
+      document.cookie=`email=${email} ; ${expires}`
+      document.cookie = `token=${token} ; ${expires}`;
+      window.location.href="/videos.html"
+ } 
+
+ const setCookie2=(email, token,newUser)=> {
+    // alert("called")
+      const d = new Date();
+      d.setTime(d.getTime() + 24*60*60*1000);
+      let expires = "expires="+ d.toUTCString();
+      document.cookie=`email=${email} ; ${expires}`
+      document.cookie = `token=${token} ; ${expires}`;
+      window.location.href="/setUp.html"
+ } 
+  
+
+const loginUser=async(Email,Password)=>{
+    try{
+        document.querySelector(".errmessage").innerHTML=""
+        document.querySelector(".submit").innerHTML="logging..."
+
+const response=await fetch("/api/login",{
+    method:"POST",
+    headers:{"content-type":"application/json"},
+    body:JSON.stringify({email:Email,password:Password })
 })
+const result=await response.json()
+
+// console.log(result)
+if(result.error){
+    document.querySelector(".submit").innerHTML="try again"
+    document.querySelector(".errmessage").innerHTML=result.errMessage
+   return
+} else{
+
+document.querySelector(".submit").innerHTML="success"
+console.log(result)
+ setCookie(result.user.email,result.token)
+}
+
+    }catch(err){
+        document.querySelector(".errmessage").innerHTML=err.message
+    }
+}
+
+
+document.querySelector(".submit").onclick=()=>{
+    // document.querySelector("#password").style.border="2px solid #fff"
+    // document.querySelector("#email").style.border="1.5px solid #fff"
+
+    const email=document.querySelector("#email").value 
+    const password=document.querySelector("#password").value
+    // const millitaryId=document.querySelector("#millitaryId").value
+    if(!email){
+        document.querySelector(".errmessage").innerHTML="your Email is required"
+        document.querySelector("#email") .style.border="2px solid red"
+        return 
+    }
+    if(!password){
+        document.querySelector(".errmessage").innerHTML="your Password is required"
+        document.querySelector("#password").style.border="2px solid red"
+        return
+    }
+    if(password.length <=7) {
+         document.querySelector(".errmessage").innerHTML="Password must be atleast 8 characters"
+         document.querySelector("#password").style.border="2px solid red"
+    }
+    // if(!millitaryId){
+    //     document.querySelector(".errmessage").innerHTML="Millitary Id is required"
+    //     document.querySelector("#millitaryId").style.border="2px solid red"
+    //    return
+    // }
+loginUser(email,password)
+}
+
+
+document.querySelector("#email").onkeyup=()=>{
+    document.querySelector(".errmessage").innerHTML=""
+    document.querySelector("#email").style.border="1.5px solid #fff"
+}
+
+document.querySelector("#password").onkeyup=()=>{
+    document.querySelector(".errmessage").innerHTML=""
+    document.querySelector("#password").style.border="1.5px solid #fff"
+}
+
+// document.querySelector("#millitaryId").onkeyup=()=>{
+//     document.querySelector(".errmessage").innerHTML=""
+//     document.querySelector("#millitaryId").style.border="1.5px solid #fff"
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
--- videoRentalFrontend/javascript/register.js
+++ videoRentalFrontend/javascript/register.js
@@ -1,61 +1,186 @@
-function setCookie(email, token) {
+// function setCookie(email, token) {
+//     // alert("called")
+//       const d = new Date();
+//       d.setTime(d.getTime() + 24*60*60*1000);
+//       let expires = "expires="+ d.toUTCString();
+//       document.cookie=`email=${email} ; ${expires}`
+//       document.cookie = `token=${token} ; ${expires}`;
+//       window.location.href="/videos.html"
+     
+//     } 
+
+
+// const registerUser=async(email,phone_number,purpose,password)=>{
+//     document.querySelector("#register").innerHTML="please wait.."
+//     try{
+
+   
+//   const response= await fetch("/api/register",{
+// method:"POST",
+// headers:{"content-type":"application/json"},
+// body:JSON.stringify({email,phone_number,purpose,password})
+//     })
+//     const result=await response.json()
+//     console.log(result)
+//     if(result.error){
+//      document.querySelector(".errmessage2").innerHTML=result.errMessage
+//      document.querySelector("#register").innerHTML="try again"
+//     }else{
+//     document.querySelector("#register").innerHTML="success"
+//     setCookie(result.message.email,result.token)
+//     // return setCookie(result.user.email,result.token)
+//     }
+
+// }catch(err){
+//     document.querySelector(".errmessage2").innerHTML=result.errmessage  
+//     document.querySelector("#register").innerHTML="try again"
+// }
+
+// }
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded",()=>{
+//     document.querySelector("#register").onclick=()=>{
+//         event.preventDefault()
+//         const email=document.querySelector(".email").value
+//         const phone_number=document.querySelector("#number").value
+//         const purpose=document.querySelector("select").value
+//         const password=document.querySelector(".password").value
+//         if(!email)return document.querySelector(".errmessage2").innerHTML="Email is required"
+//         if(!phone_number)return document.querySelector(".errmessage2").innerHTML="phone number is required"
+//         if(!purpose)return document.querySelector(".errmessage2").innerHTML="your purpose of buying videos is required"
+//         if(!password)return document.querySelector(".errmessage2").innerHTML="password is required"
+//         if(password.length < 8)return document.querySelector(".errmessage2").innerHTML="password length must be greater than 8"
+//         document.querySelector(".errmessage2").innerHTML=""
+//         registerUser(email,phone_number,purpose,password)
+//     }
+// })
+
+
+
+
+
+
+const setCookie=(email, token)=> {
     // alert("called")
       const d = new Date();
-      d.setTime(d.getTime() + 24*60*60*1000);
+      d.setTime(d.getTime() + 24*60*60*100);
       let expires = "expires="+ d.toUTCString();
       document.cookie=`email=${email} ; ${expires}`
       document.cookie = `token=${token} ; ${expires}`;
-      window.location.href="/dashboard.html"
-     
+      window.location.replace("/videos.html")
     } 
+  
+  
 
 
-const registerUser=async(email,phone_number,purpose,password)=>{
-    document.querySelector("#register").innerHTML="please wait.."
+const registeruser=async(Email,fullName,PhoneNumber,purpose,Password)=>{
+    const input=document.querySelectorAll("input")
+input.forEach(input=>input.style.border="1px solid rgb(36, 35, 35)")
     try{
-
-   
-  const response= await fetch("/api/register",{
-method:"POST",
-headers:{"content-type":"application/json"},
-body:JSON.stringify({email,phone_number,purpose,password})
+     document.querySelector(".submit").innerHTML="Proccessing"
+    const response=await fetch("/api/register",{
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({email:Email,phone_number:PhoneNumber,purpose,password:Password })
     })
     const result=await response.json()
     console.log(result)
-    if(result.error){
-     document.querySelector(".errmessage2").innerHTML=result.errMessage
-     document.querySelector("#register").innerHTML="try again"
-    }else{
-    document.querySelector("#register").innerHTML="success"
+   if(result.error){
+    document.querySelector(".errmessage").innerHTML=result.errMessage
+    document.querySelector(".submit").innerHTML="Try again"
+   } else{
+    document.querySelector(".submit").innerHTML="Success"
     setCookie(result.message.email,result.token)
-    // return setCookie(result.user.email,result.token)
-    }
+    
+   }
 
 }catch(err){
-    document.querySelector(".errmessage2").innerHTML=result.errmessage  
-    document.querySelector("#register").innerHTML="try again"
+    document.querySelector(".errmessage").innerHTML=err.message
 }
-
-}
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded",()=>{
-    document.querySelector("#register").onclick=()=>{
-        event.preventDefault()
-        const email=document.querySelector(".email").value
-        const phone_number=document.querySelector("#number").value
-        const purpose=document.querySelector("select").value
-        const password=document.querySelector(".password").value
-        if(!email)return document.querySelector(".errmessage2").innerHTML="Email is required"
-        if(!phone_number)return document.querySelector(".errmessage2").innerHTML="phone number is required"
-        if(!purpose)return document.querySelector(".errmessage2").innerHTML="your purpose of buying videos is required"
-        if(!password)return document.querySelector(".errmessage2").innerHTML="password is required"
-        if(password.length < 8)return document.querySelector(".errmessage2").innerHTML="password length must be greater than 8"
-        document.querySelector(".errmessage2").innerHTML=""
-        registerUser(email,phone_number,purpose,password)
     }
-})
+    
+
+
+document.querySelector(".submit").onclick=()=>{
+    const email=document.querySelector("#email").value 
+    const phoneNumber=document.querySelector("#phoneNumber").value 
+    const purpose=document.querySelector("select").value
+    // const millitaryId=document.querySelector("#millitaryId").value
+    const password=document.querySelector("#password").value
+    const confirmPassword=document.querySelector("#password2").value
+    const fullName=document.querySelector("#fullName").value
+
+    if(!email) { 
+        document.querySelector(".errmessage").innerHTML="Email is required" 
+        document.querySelector("#email") .style.border="2px solid red"
+        return
+}   
+if(!fullName) { 
+    document.querySelector(".errmessage").innerHTML="full name is required" 
+    document.querySelector("#fullName") .style.border="2px solid red"
+    return
+}  
+    
+    if(!phoneNumber){
+        document.querySelector(".errmessage").innerHTML="Phone number is required"
+        document.querySelector("#phoneNumber") .style.border="2px solid red"
+        return
+    }
+
+    if(!purpose){
+        document.querySelector(".errmessage").innerHTML="Please select your purpose of buying videos"
+        document.querySelector("select") .style.border="2px solid red"
+        return
+    }
+
+    // if(!millitaryId){
+    //     document.querySelector(".errmessage").innerHTML="Millitary Id is required"
+    //     document.querySelector("#millitaryId").style.border="2px solid red"
+    //    return
+    // }
+
+    if(!password){
+        document.querySelector(".errmessage").innerHTML="Password is required"
+        document.querySelector("#password").style.border="2px solid red"
+return
+    }
+    if(password.length<=7){
+        document.querySelector(".errmessage").innerHTML="Password must be atleast 8 characters"
+        document.querySelector("#password").style.border="2px solid red"
+return
+    }
+
+    if(!confirmPassword){
+        document.querySelector(".errmessage").innerHTML="Please confirm Password"
+        document.querySelector("#password2").style.border="2px solid red"
+return
+    }
+
+    if(password !== confirmPassword){
+        document.querySelector(".errmessage").innerHTML="Password must match"
+        document.querySelector("#password2").style.border="2px solid red"
+        document.querySelector("#password").style.border="2px solid red"
+return
+    }
+
+    registeruser(email,fullName,phoneNumber,purpose,password)
+    
+}
+
+
+document.onkeyup=(event)=>{
+    event.target.style.border="1px solid rgb(36, 35, 35)"
+    document.querySelector(".errmessage").innerHTML=""
+}
+document.querySelector("select").onchange=(event)=>{
+    event.target.style.border="1px solid rgb(36, 35, 35)"
+    document.querySelector(".errmessage").innerHTML=""
+}
+
+
+
